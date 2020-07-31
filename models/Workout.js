84eb16a3b4bea518bfba237d483e4f83437c1918
Robert Luttig <robert.luttig@gmail.com>
2020-07-30 17:56:29 -0700

    created initial models folder with workout and index .js

---
+++ models/Workout.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date
+  },
+  exercises: {
+    type: []
+  },
+});
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
+// day: new Date(new Date().setDate(new Date().getDate() - 10)),
+// exercises: [
+//   {
+//     type: "resistance",
+//     name: "Bicep Curl",
+//     duration: 20,
+//     weight: 100,
+//     reps: 10,
+//     sets: 4
+//   }
+// ]
+// },
