8ab8a1db8e91d70c84fa43b84d5af4f1e6c0bfa7
Robert Luttig <robert.luttig@gmail.com>
2020-08-02 15:00:50 -0700

    all routes besides range work now

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,61 @@
-// const db = require("../models");
+const db = require("../models");
 
-// module.exports = (app) => {
-//     app.get("/api/workouts", (req, res) => {
-//       //TODO
-//       // 1. Add a join to include all of each Author's Posts
-//       db.Workout.findAll({
-//         include:[db.WorkoutSchema
-//       }).then((dbWorkout) => {
-//         res.json(dbWorkout);
-//       });
-//     });
-// };
+
+// get all workouts
+module.exports = (app) => {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+    });
+
+    // post method to create workout
+
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create({})
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+    });
+    // adding a put method to update a workout
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+			 req.params.id,
+			{ $push: { exercises: req.body } },
+			{ new: true, runValidators: true }
+		)
+			.then((data) => {
+				res.json(data);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+    });
+
+    // // get workouts in range
+    // app.get("/api/workouts/range", (req, res) => {
+    //     db.Workout.find({})
+        //     .sort({ date: -1 })
+        //     .then((data) => {
+        //         res.send(data);
+        //     })
+        //     .catch((err) => {
+        //         res.status(400).json(err);
+        //     });
+        // });
+       
+    //   });
+    
+};
+
+
+
+
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-// const addPostApiRoutes = require("./routes/post-api-routes.js");
-// const addAuthorApiRoutes = require("./routes/author-api-routes.js");
+const addApiRoutes = require("./routes/api-routes.js");
 const addHtmlRoutes = require("./routes/html-routes.js");
 const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
@@ -12,33 +11,27 @@ mongoose.connect("mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
+// Sets up the Express app to handle data parsing
 const app = express();
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"))
-// Requiring our models for syncing
-// const db = require("./models");
 
-// Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
 
 // Static directory
 // app.use(express.static("public"));
 
 // Routes
-// addAuthorApiRoutes(app);
-// addPostApiRoutes(app);
- addHtmlRoutes(app);
+addApiRoutes(app);
+addHtmlRoutes(app);
 
 // app.get("*", (req, res) =>{
 //     res.send("<h1>Hello, World!</h1>");
 // });
 
-// Syncing our sequelize models and then starting our express app
-// db.sequelize.sync({ force: false }).then(() => {
+
   app.listen(PORT, () => {
     console.log(`App listening on PORT ${ PORT}`);
   });
-// });
+
