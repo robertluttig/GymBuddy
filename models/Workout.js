b8928eddf0fe015ddd6d2d46a7ed6145b79ae653
Robert Luttig <robert.luttig@gmail.com>
2020-08-04 09:56:49 -0700

    updated workout js to reflect duration using virtual

--- models/Workout.js
+++ models/Workout.js
@@ -8,29 +8,22 @@ const WorkoutSchema = new Schema({
   exercises: {
     type: []
   }
-  // ,
-  // toJSON:
-  //  { virtuals: true } 
+},
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+
+
+WorkoutSchema.virtual('totalDuration').get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
-// Create a virtual property `domain` that's computed from `email`.
-// WorkoutSchema.virtual('totalDuration').get(function() {
-//   return this.exercises.add(this.exercises.totalDuration + 1);
-// });
 
-// const
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 module.exports = Workout;
-// day: new Date(new Date().setDate(new Date().getDate() - 10)),
-// exercises: [
-//   {
-//     type: "resistance",
-//     name: "Bicep Curl",
-//     duration: 20,
-//     weight: 100,
-//     reps: 10,
-//     sets: 4
-//   }
-// ]
-// },
